c930d407b540bc2c744c5c225e5ae1a1dbc18804
RCN <ravindunishshankawork@gmail.com>
2025-07-30 00:19:02 +0530

    just added the topic and the select box

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <h1>Customer Management</h1>
+        <select name="status">
+            <option value="active" >Active</option>
+            <option value="banned">Banned</option>
+            <option value="allStatus" selected>All Status</option>
+        </select>
+    </body>
+</html>
